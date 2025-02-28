2f05306abdef1ececa5b743bb7920359d0776ad6
Telphii <zambezikrv@yandex.com>
2025-02-28 23:31:32 +0700

    Style column and taskColumn1

--- js/app.js
+++ js/app.js
@@ -19,3 +19,27 @@ const taskManager = createApp({
 });
 
 taskManager.mount('#app');
+
+const TaskColumn = {
+    props: {
+        columnID: Number,
+        taskCards: Array,
+    },
+    template: `
+        <div class="column">
+            <h2>Столбец {{ columnID }}</h2>
+            <div v-for="(card, index) in taskCards" :key="index">
+                <div class="card">
+                    <h3>{{ card.title }}</h3>
+                    <ul>
+                        <li v-for="(item, i) in card.list" :key="i">
+                            {{ item.text }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `,
+};
+
+taskManager.component('TaskColumn', TaskColumn);
